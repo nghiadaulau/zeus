27bf43c3b2d68bc0eeb5636bfe9e08169b2eea33
nghiadaulau <nhatnghiatyper@gmail.com>
2023-04-16 13:22:34 +0700

    complete login admin

--- admin/src/main/java/vn/tdtu/edu/admin/AdminApplication.java
+++ admin/src/main/java/vn/tdtu/edu/admin/AdminApplication.java
@@ -5,9 +5,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import vn.tdtu.edu.commons.dto.AdminDTO;
+import vn.tdtu.edu.commons.model.Admin;
 import vn.tdtu.edu.commons.model.Role;
+import vn.tdtu.edu.commons.repository.AdminRepository;
 import vn.tdtu.edu.commons.repository.RoleRepository;
+import vn.tdtu.edu.commons.service.implement.AdminServiceImpl;
+
+import java.util.Collections;
 
 @SpringBootApplication(scanBasePackages = {"vn.tdtu.edu.commons.*", "vn.tdtu.edu.admin.*"})
 @EnableJpaRepositories(value = "vn.tdtu.edu.commons.repository")
@@ -15,6 +23,7 @@ import vn.tdtu.edu.commons.repository.RoleRepository;
 public class AdminApplication implements CommandLineRunner {
 	@Autowired
 	private RoleRepository roleRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
