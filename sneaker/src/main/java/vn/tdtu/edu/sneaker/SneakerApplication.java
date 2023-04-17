42363482deb3284228f1b0ac909356331ebf3a76
WiBuMoney <cowsmile1911@gmail.com>
2023-04-17 22:21:34 +0700

    Cập nhật login,regis,information template

--- sneaker/src/main/java/vn/tdtu/edu/sneaker/SneakerApplication.java
+++ sneaker/src/main/java/vn/tdtu/edu/sneaker/SneakerApplication.java
@@ -2,16 +2,10 @@ package vn.tdtu.edu.sneaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import vn.tdtu.edu.commons.model.Role;
-import vn.tdtu.edu.commons.repository.RoleRepository;
 
-@SpringBootApplication(scanBasePackages = {"vn.tdtu.edu.commons.*"})
+@SpringBootApplication(scanBasePackages = {"vn.tdtu.edu.commons.*","vn.tdtu.edu.sneaker.*"})
 @EnableJpaRepositories(value = "vn.tdtu.edu.commons.repository")
 @EntityScan(value = "vn.tdtu.edu.commons.model")
 public class SneakerApplication {
