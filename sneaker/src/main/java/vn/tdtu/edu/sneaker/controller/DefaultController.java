f1a540676799651c1ca4c43224fa0ef4ab8052bd
TranVanThao <vanthao000009@gmail.com>
2023-04-20 18:10:55 +0700

    Update DefaultController.java

--- sneaker/src/main/java/vn/tdtu/edu/sneaker/controller/DefaultController.java
+++ sneaker/src/main/java/vn/tdtu/edu/sneaker/controller/DefaultController.java
@@ -1,6 +1,9 @@
 package vn.tdtu.edu.sneaker.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class DefaultController {
     @GetMapping("/home-page")
-    public String Index(){
+    public String Index(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication.isAuthenticated()){
+            model.addAttribute("username",authentication.getName());
+        }
         return "index";
     }
 }
